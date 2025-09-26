523d5fbee8f2e707d97ca75f68127dffdf8170f3
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-26 11:22:00 +0530

    landing page fonts

--- landing/src/app/layout.tsx
+++ landing/src/app/layout.tsx
@@ -1,15 +1,23 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { DM_Sans, Khula, Catamaran } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const dmSans = DM_Sans({
   subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-dm-sans",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const khula = Khula({
   subsets: ["latin"],
+  weight: ["400", "600", "700", "800"],
+  variable: "--font-khula",
+});
+
+const catamaran = Catamaran({
+  subsets: ["latin"],
+  weight: ["400", "600", "700", "800"],
+  variable: "--font-catamaran",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${dmSans.variable} ${khula.variable} ${catamaran.variable}`}
       >
         {children}
       </body>

3ea80ae24741d3f1587c37ce64099bdbc086ba72
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-10-01 21:22:58 +0530

    navbar comp

--- frontend/src/Component/Navbar.tsx
+++ frontend/src/Component/Navbar.tsx
@@ -13,7 +13,6 @@ const Navbar: React.FC = () => {
 
   return (
     <nav className="sticky top-0 z-50 w-full bg-gradient-to-r from-gray-900 to-gray-800 backdrop-blur-md shadow-xl px-6 py-4 flex items-center justify-between">
-      {/* Left: Greeting */}
       <div
         className="text-white font-semibold text-lg hover:text-cyan-400 transition-colors cursor-pointer"
         onClick={() => navigate("/dashboard")}
@@ -21,7 +20,6 @@ const Navbar: React.FC = () => {
         {username ? `Hello, ${username}` : "Hello, Guest"}
       </div>
 
-      {/* Right: Logout */}
       <button
         onClick={handleLogout}
         className="px-4 py-2 rounded-xl hover:scale-105 transition-all duration-200  text-white font-semibold text-sm"

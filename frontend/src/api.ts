030b05f4b36bfb4f7730521ef3b2f1a0560c19f0
komal-kumawat <kumawatkomal17122004@gmail.com>
2025-10-01 11:30:54 +0530

    deployed backend

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/v0",
+  baseURL: "https://meshspire.onrender.com/api/v0",
   headers: {
     "Content-Type": "application/json",
   },
